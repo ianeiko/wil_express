0c3f34ac1ea0cb7b6190a976b2afa5167c544b53
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 16:03:49 -0700

    working version

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
 var express = require('express')
-// varjmongoose = require("mongoose")
+var mongoose = require('mongoose')
 
-// Connect to mongodb
-// var MONGOHQ_URL="mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test"
-// var connect = function () {
-//   var options = { server: { socketOptions: { keepAlive: 1 } } };
-//   mongoose.connect(MONGOHQ_URL, options)
-// };
-// connect();
+mongoose.connect("mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test");
 
-// mongoose.connection.on('error', console.log);
-// mongoose.connection.on('disconnected', connect);
-// var db = mongoose.connection
-
-// init Express
 var app = express()
-var port = process.env.PORT || 5000;
 
-// MODELS
-// Records = mongoose.model('records', { station: String, temperature: Array })
-
-
-// ROUTES
 app.get('/', function (req, res) {
-  return res.send('Hey, Dad!')
+  return res.send('Hey, yo!')
 })
 
-// app.get('/list', function (req, res) {
-//   Records.find({}, function(err, documents) {
-//     return res.json(documents)
-//   })
-// })
+Records = mongoose.model('records', { station: String, temperature: Array })
+app.get('/list', function (req, res) {
+  Records.find({}, function(err, documents) {
+    return res.json(documents)
+  })
+})
 
-app.listen(port)
+var port = process.env.PORT || 5000
+app.listen(port);
