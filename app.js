63eab89ef4388539898f705ff40684cfced6aced
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 16:21:53 -0700

    ask end-point

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var routes = require('./routes')
 var mongoose = require('mongoose')
 
 mongoose.connect("mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test");
@@ -9,6 +10,8 @@ app.get('/', function (req, res) {
   return res.send('Hey, yo!')
 })
 
+app.get('/ask', routes.ask)
+
 Records = mongoose.model('records', { station: String, temperature: Array })
 app.get('/list', function (req, res) {
   Records.find({}, function(err, documents) {
@@ -16,5 +19,5 @@ app.get('/list', function (req, res) {
   })
 })
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 3000
 app.listen(port);
---
+++ routes/index.js
@@ -0,0 +1,3 @@
+exports.ask = function(req, res) {
+  res.end('ASK');
+}
