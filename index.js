e8e18c075da04dd42320f2f7cfd9491de18c2b98
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 15:16:26 -0700

    no db

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var express = require('express')
-var mongoose = require("mongoose")
+// varjmongoose = require("mongoose")
 
 // Connect to mongodb
-var MONGOHQ_URL="mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test"
-var connect = function () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  mongoose.connect(MONGOHQ_URL, options)
-};
-connect();
+// var MONGOHQ_URL="mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test"
+// var connect = function () {
+//   var options = { server: { socketOptions: { keepAlive: 1 } } };
+//   mongoose.connect(MONGOHQ_URL, options)
+// };
+// connect();
 
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
-var db = mongoose.connection
+// mongoose.connection.on('error', console.log);
+// mongoose.connection.on('disconnected', connect);
+// var db = mongoose.connection
 
 // init Express
 var app = express()
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 // MODELS
-Records = mongoose.model('records', { station: String, temperature: Array })
+// Records = mongoose.model('records', { station: String, temperature: Array })
 
 
 // ROUTES
@@ -26,10 +26,10 @@ app.get('/', function (req, res) {
   return res.send('Hey, Dad!')
 })
 
-app.get('/list', function (req, res) {
-  Records.find({}, function(err, documents) {
-    return res.json(documents)
-  })
-})
+// app.get('/list', function (req, res) {
+//   Records.find({}, function(err, documents) {
+//     return res.json(documents)
+//   })
+// })
 
 app.listen(port)
