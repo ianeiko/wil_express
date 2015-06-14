491321265292448e7a61eba08ffe64e26ecc4a79
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 18:04:21 -0700

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var routes = require('./routes')
-var mongoose = require('mongoose')
+// var mongoose = require('mongoose')
 
-mongoose.connect("mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test");
+// mongoose.connect("mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test");
 
 var app = express()
 
@@ -14,12 +14,12 @@ app.get('/', function (req, res) {
 
 app.get('/ask', routes.ask)
 
-Records = mongoose.model('records', { station: String, temperature: Array })
-app.get('/list', function (req, res) {
-  Records.find({}, function(err, documents) {
-    return res.json(documents)
-  })
-})
+// Records = mongoose.model('records', { station: String, temperature: Array })
+// app.get('/list', function (req, res) {
+//   Records.find({}, function(err, documents) {
+//     return res.json(documents)
+//   })
+// })
 
 
 var port = process.env.PORT || 3000
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var _ = require('lodash')
-var stringify = require('json-stringify-safe');
+// var stringify = require('json-stringify-safe');
 
 exports.ask = function(req, res) {
   return res.send('Hey, yo!')
