c08bd37cf5633b0a41ff9b18ad68f51165a13661
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 18:14:31 -0700

    test

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 var express = require('express')
 var routes = require('./routes')
-// var mongoose = require('mongoose')
-
-// mongoose.connect("mongodb://wil_test:nonuvlutonm@c1104.candidate.11.mongolayer.com:11104/wil_test");
 
 var app = express()
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   return res.send('Hey, yo!')
 })
 
 app.get('/ask', routes.ask)
-
-// Records = mongoose.model('records', { station: String, temperature: Array })
-// app.get('/list', function (req, res) {
-//   Records.find({}, function(err, documents) {
-//     return res.json(documents)
-//   })
-// })
-
 
 var port = process.env.PORT || 3000
 app.listen(port);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var fs = require('fs');
 var _ = require('lodash')
-// var stringify = require('json-stringify-safe');
+var stringify = require('json-stringify-safe');
 
 exports.ask = function(req, res) {
-  return res.send('Hey, yo!')
+  var data = stringify(req.headers)
+  fs.writeFile("./public/twilio.json", data, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
 }
