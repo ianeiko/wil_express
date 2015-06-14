b5889c4e5b0399028d552bacf06184d03711b362
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 18:02:58 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,5 @@ var _ = require('lodash')
 var stringify = require('json-stringify-safe');
 
 exports.ask = function(req, res) {
-  var data = stringify(req.headers)
-  fs.writeFile("./public/twilio.json", data, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("The file was saved!");
-  });
+  return res.send('Hey, yo!')
 }
