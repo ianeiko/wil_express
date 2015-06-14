38df250c0e04d1e87c729500022fadb0a9a98021
Oleg Janeiko <jneiku@gmail.com>
2015-06-13 17:55:19 -0700

    twilio script

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var _ = require('lodash')
+var stringify = require('json-stringify-safe');
+
 exports.ask = function(req, res) {
-  res.end('ASK');
+  var data = stringify(req.headers)
+  fs.writeFile("./public/twilio.json", data, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
 }
